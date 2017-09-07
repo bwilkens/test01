db3a73bde1e46fd13dbd5f97c2826d4367a23f4d
berend.wilkens <berend.wilkens@hu.nl>
2017-09-07 11:36:10 +0200

    first commit

---
+++ les_1/exercise_1_1.py
@@ -0,0 +1,5 @@
+a=5
+print("{0:5} - {0:5} - {0:5}".format(str(a), a, str(type(a))))
+print("{0:5} - {0:5} - {0:5}".format("5.0", 5.0, type(5.0)))
+print("5.0",5.0,type(5.0))
+print(" 5%2   - "+str(5%2)+" - "+ str(type(5%2)))
